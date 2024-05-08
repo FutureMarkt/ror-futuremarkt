167a6c4dd54230eb363e3b77e27f07b515aad7b8
IgorRusskikh <russkikh200@mail.ru>
2024-05-08 13:35:46 +0700

    Разработана десктоп версия раздела 'ЧЕМ МЫ МОЖЕМ ВАМ ПОМОЧЬ'

--- src/components/Layout/OurHelp.tsx
+++ src/components/Layout/OurHelp.tsx
@@ -1,3 +1,5 @@
+import { affectBold700, ppNeueMontLight500 } from '@/utils/fonts';
+
 import Section from '../Section';
 
 const OurHelp = () => {
@@ -21,8 +23,24 @@ const OurHelp = () => {
         </div>
 
         {/* LIST OF SERVICES */}
-        <div>
-          
+        <div className="mt-[40px] grid grid-cols-3 gap-5">
+          {[1, 2, 3, 4, 5, 6].map((item) => (
+            <div
+              key={item}
+              className="max-w-[420px] border border-[#030303] rounded-[5px] px-[17px] py-[20px]"
+            >
+              <h3 className={`text-xl ${affectBold700.className}`}>
+                Разработка веб-проектов
+              </h3>
+              <div
+                className={`mt-4 leading-[16.1px] text-xs text-[#4C4C4C] ${ppNeueMontLight500.className}`}
+              >
+                Создание качественных и масштабируемых веб-приложений
+                с использованием мощного и гибкого фреймворка - Ruby on Rails
+              </div>
+              <div className="mt-9 leading-[16.1px] text-xs">от $00 000</div>
+            </div>
+          ))}
         </div>
       </div>
     </Section>
