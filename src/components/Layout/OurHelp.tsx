5783804f4f6b95bb833e5dfe1f13987cc71f496c
IgorRusskikh <russkikh200@mail.ru>
2024-05-18 17:27:28 +0700

    Вывод карточек переписан с учетом файла локализации. Усложнен цикл

--- src/components/Layout/OurHelp.tsx
+++ src/components/Layout/OurHelp.tsx
@@ -1,52 +1,98 @@
-import { affectBold700, ppNeueMontLight500 } from '@/utils/fonts';
+"use client";
+
+import { useTranslations } from 'next-intl';
+import { useState } from 'react';
+
+import { affectBold700, ppNeueMont500 } from '@/utils/fonts';
 
 import Section from '../Section';
 
+interface Projects {
+  [key: string]: any;
+}
+
+interface Filters {
+  [filter: string]: Projects;
+}
+
 const OurHelp = () => {
+  const ourHelpIntl = useTranslations("Index.OurHelp");
+
   const id = "our-help";
-  const header = "чем мы можем вам помочь";
+  const header = ourHelpIntl.raw("title");
+
+  const [selectedFilter, setSelectedFilter] = useState(
+    "Проектная деятельность"
+  );
+
+  const filters = Object.entries(ourHelpIntl.raw("filters") as Filters);
 
   return (
     <Section id={id} header={header} theme="light">
       <div>
         {/* SERVICE FILTER */}
         <div className="flex gap-[30px] justify-center">
-          {filters.map((filter) => (
-            <a
+          {filters.map(([filter, projects]) => (
+            <button
               key={filter}
-              href=""
-              className="leading-6 border-b-2 border-[#030303]"
+              className={`leading-6 border-b-2  ${
+                filter === selectedFilter
+                  ? "text-[#030303] border-[#030303]"
+                  : "text-[#A0A0A0] border-[#A0A0A0]"
+              } `}
+              onClick={() => setSelectedFilter(filter)}
             >
-              {filter} [0]
-            </a>
+              {filter} [{Object.keys(projects).length}]
+            </button>
           ))}
         </div>
 
         {/* LIST OF SERVICES */}
         <div className="mt-[40px] grid grid-cols-3 gap-5">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-            <div
-              key={item}
-              className="max-w-[420px] border border-[#030303] rounded-[5px] px-[17px] py-[20px]"
-            >
-              <h3 className={`text-xl ${affectBold700.className}`}>
-                Разработка веб-проектов
-              </h3>
-              <div
-                className={`mt-4 leading-[16.1px] text-xs text-[#4C4C4C] ${ppNeueMontLight500.className}`}
-              >
-                Создание качественных и масштабируемых веб-приложений
-                с использованием мощного и гибкого фреймворка - Ruby on Rails
-              </div>
-              <div className="mt-9 leading-[16.1px] text-xs">от $00 000</div>
-            </div>
-          ))}
+          {selectedFilter &&
+            filters.map(([filter, cards]) => {
+              if (selectedFilter === filter) {
+                return Object.entries(cards).map(
+                  ([cardTitle, { description, price }]) => {
+                    return (
+                      <div
+                        key={cardTitle}
+                        className="lg:max-w-[301px] 2xl:max-w-[420px] border border-[#030303] rounded-[5px] px-[17px] py-[20px]"
+                      >
+                        <h3
+                          className={`lg:text-[22px] 2xl:text-xl lg:leading-5 2xl:leading-6 ${affectBold700.className}`}
+                        >
+                          {cardTitle}
+                        </h3>
+                        <div
+                          className={`mt-4 leading-[16.1px] text-xs text-[#4C4C4C] marker: ${ppNeueMont500.className}`}
+                        >
+                          <ul className=" list-[square]">
+                            {/* CHECK IF THERE IS MORE THAN ONE ELEMENT IN THE OBJECT — IT OUTPUTS A BULLETED LIST, OTHERWISE WE JUST OUTPUT A DESCRIPTION */}
+                            {description.split("; ").length > 1
+                              ? description
+                                  .split("; ")
+                                  .map((desc: string, index: number) => (
+                                    <li key={index} className="ml-[17px]">
+                                      {desc}
+                                    </li>
+                                  ))
+                              : description}
+                          </ul>
+                        </div>
+                        <div className="mt-9 leading-[16.1px] text-xs">
+                          {ourHelpIntl.raw("from")} ${price}
+                        </div>
+                      </div>
+                    );
+                  }
+                );
+              }
+            })}
         </div>
       </div>
     </Section>
   );
 };
-
-const filters = ["Проектная деятельность", "Аутстаффинг"];
 
 export default OurHelp;
