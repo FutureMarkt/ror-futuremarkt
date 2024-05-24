3aeb6ad8d201bb63575edf2e08f24009181680db
IgorRusskikh <russkikh200@mail.ru>
2024-05-24 19:54:24 +0700

    Добавлена кастомизация извне

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,16 +7,27 @@ interface CardProps {
   content: string;
   children?: ReactNode;
   classes?: string;
+  headerStyles?: string;
+  contentStyles?: string;
 }
 
-const Card = ({ title, content, children, classes }: CardProps) => {
+const Card = ({
+  title,
+  content,
+  children,
+  classes,
+  headerStyles,
+  contentStyles,
+}: CardProps) => {
   return (
     <div
-      className={`text-[#030303] max-w-[420px] w-[420px] border rounded-[5px] bg-[#DFDFDF]  ${classes}`}
+      className={`text-[#030303] 2xl:max-w-[420px] 2xl:w-[420px] border rounded-[5px] bg-[#DFDFDF]  ${classes}`}
     >
-      <h4 className={`uppercase ${affectBold700.className}`}>{title}</h4>
+      <h4 className={`uppercase ${affectBold700.className} ${headerStyles}`}>
+        {title}
+      </h4>
       <p
-        className={`text-xs text-[#4C4C4C] mt-4 leading-[16.1px] ${ppNeueMont500.className}`}
+        className={`text-[#4C4C4C] mt-4 leading-[16.1px] ${ppNeueMont500.className} ${contentStyles}`}
       >
         {content}
       </p>
