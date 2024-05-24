a04ad9d4babb14fe093cf4e459fe83647df58af0
IgorRusskikh <russkikh200@mail.ru>
2024-05-24 19:51:22 +0700

    Создан компонент с выпадающим меню шапки
    - сверстан блок, находящийся в шапке, с навигационным меню и переключателем языка
    - расположены изображения рубинов и написаны медиа запросы

---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,83 @@
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import { useContext } from 'react';
+
+import { ppNeueMont500 } from '@/utils/fonts';
+
+import LocaleSwitcher from './LocalSwitcher';
+import { MenuContext } from './MenuContext';
+
+const DropdownMenu = () => {
+  const headerIntl = useTranslations("Index.Header");
+
+  const { isOpen, setIsOpen } = useContext(MenuContext);
+
+  return (
+    <div
+      className={`absolute w-full h-[100vh] z-20 transition-all text-[#F7F7F7] bg-[#030303] overflow-hidden ${
+        isOpen ? "top-0" : "top-[-100%]"
+      }`}
+    >
+      <Image
+        src="/ruby.png"
+        width={150}
+        height={150}
+        alt=""
+        className="absolute mt-[160px] left-[20px] -rotate-[9deg] hidden md:block"
+      />
+      <Image
+        src="/ruby.png"
+        width={300}
+        height={300}
+        alt=""
+        className="absolute mt-[460px] -right-[70px] rotate-[15deg] hidden md:block"
+      />
+      <Image src={"/_header/dropdown-menu-bg.png"} fill alt="" />
+      <div className="absolute z-30 w-full h-full px-[15px] md:px-[30px] py-[18px] md:py-[30px]">
+        <div className="flex justify-between">
+          <div
+            onClick={() => {
+              setIsOpen(false);
+              document.body.style["overflowY"] = "scroll";
+            }}
+          >
+            <Image
+              src="/_header/close-icon.svg"
+              width={22}
+              height={22}
+              alt="close-icon"
+            />
+          </div>
+
+          <a
+            href="#"
+            className={`text-[#FFDE9F] text-xs md:text-lg flex items-center cursor-pointe uppercase ${ppNeueMont500.className}`}
+          >
+            [{headerIntl.raw("messageUs")}
+            <Image
+              src={"/arrow-side-yellow.png"}
+              width={16}
+              height={16}
+              alt="arrow"
+              className="mx-[2px] w-[11.33px] md:w-[16px] h-[11.33px] md:h-[16px]"
+            />
+            ]
+          </a>
+        </div>
+
+        <div
+          className={`mt-[210px] md:mt-[65px] flex flex-col items-center gap-[30px] md:gap-[50px] text-sm md:text-[48px] leading-[24px] md:leading-[48px] ${ppNeueMont500.className}`}
+        >
+          <a href="#">[Услуги]</a>
+          <a href="#">[Услуги]</a>
+          <a href="#">[Преимущества RoR]</a>
+          <a href="#">[Услуги]</a>
+        </div>
+
+        <LocaleSwitcher />
+      </div>
+    </div>
+  );
+};
+
+export default DropdownMenu;
