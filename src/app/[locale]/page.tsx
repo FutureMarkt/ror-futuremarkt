891ddf0382ea1c6dea4ef3c2d57e3122b9f7a025
IgorRusskikh <russkikh200@mail.ru>
2024-05-24 19:50:19 +0700

    Перенесены файлы
    Файлы перенесены для подготовки приложения к созданию локализации

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,23 @@
+import AboutUs from '@/components/Layout/AboutUs';
+import Footer from '@/components/Layout/Footer';
+import Header from '@/components/Layout/Header';
+import OurCases from '@/components/Layout/OurCases';
+import OurHelp from '@/components/Layout/OurHelp';
+import AdvantagesROR from '@/components/Layout/RORAdvantages';
+import WeInMedia from '@/components/Layout/WeInMedia';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <OurHelp />
+        <OurCases />
+        <AdvantagesROR />
+        <AboutUs />
+        <WeInMedia />
+      </main>
+      <Footer />
+    </div>
+  );
+}
