6581ffafee8f912014d514f8840378027c6ccb87
IgorRusskikh <russkikh200@mail.ru>
2024-05-27 15:09:21 +0700

    Измене файл с разделом "Мы в медиа"
    - добавлена зависимость стилей от локализации. Необходимо для иврит
      версии
    - добавлены ссылки на статьи Егора
    - добавлены медиа запросы для изображений из статей
    - прописаны новые медиа запросы
    - создан цикл для рендера ссылок на социальную медиа

--- src/components/Layout/WeInMedia.tsx
+++ src/components/Layout/WeInMedia.tsx
@@ -1,5 +1,6 @@
 import { useLocale, useTranslations } from 'next-intl';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { affectBold700, affectLight300, ppNeueMont500 } from '@/utils/fonts';
 
@@ -17,11 +18,15 @@ const WeInMedia = () => {
   return (
     <Section id={id} header={header} theme="light">
       {/* OUR MEDIA  */}
-      <div className="flex flex-col md:flex-row gap-[30px] mt-[30px] md:mt-0">
+      <div
+        className={`flex flex-col gap-[30px] mt-[30px] md:mt-0 ${
+          locale === "he" ? "md:flex-row-reverse" : "md:flex-row"
+        }`}
+      >
         {/* ARTICLES */}
         {Object.entries(weInMediaIntl.raw("articles")).map(
           // @ts-ignore
-          ([title, { img, description }], index) => (
+          ([title, { img, description, link }], index) => (
             <div
               key={index}
               className="flex flex-col md:max-w-[334px] lg:max-w-[466px] 2xl:max-w-[635px]"
@@ -32,17 +37,28 @@ const WeInMedia = () => {
                   fill
                   placeholder="empty"
                   alt={img.split(".")[0]}
-                  className="hidden md:block"
+                  className="hidden 2xl:block"
+                />
+                <Image
+                  src={`/_we-in-media/${locale}/${img}-lg.png`}
+                  fill
+                  placeholder="empty"
+                  alt={img.split(".")[0]}
+                  className="hidden lg:block 2xl:hidden"
                 />
                 <Image
                   src={`/_we-in-media/${locale}/${img}-mobile.png`}
                   fill
                   placeholder="empty"
                   alt={img.split(".")[0]}
-                  className="block md:hidden"
+                  className="block lg:hidden"
                 />
               </div>
-              <div className="mt-3 h-[156px] flex flex-col justify-between">
+              <div
+                className={`mt-3 h-[152px] md:h-[156px] lg:h-[140px] xl:h-[100px] flex flex-col justify-between ${
+                  locale === "he" ? "text-left" : ""
+                }`}
+              >
                 <h4
                   className={`uppercase text-xl leading-[24px] ${affectBold700.className}`}
                 >
@@ -54,11 +70,12 @@ const WeInMedia = () => {
                   >
                     {description}
                   </p>
-                  <a
+                  <Link
+                    href={`${link}`}
                     className={`text-xs leading-[16.1px] mt-6 ${ppNeueMont500.className}`}
                   >
                     [{weInMediaIntl.raw("readMore")}]
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -67,22 +84,26 @@ const WeInMedia = () => {
       </div>
 
       {/* VIDEO */}
-      <div className="mt-[30px] md:mt-[60px]">
+      <div className="mt-[30px] md:mt-[60px] hidden">
         <div className="w-330px md:w-[708px] lg:w-[964px] 2xl:w-[1300px] h-[186px] md:h-[398px] lg:h-[542px] 2xl:h-[731px] bg-[#030303] rounded-[5px]"></div>
       </div>
 
       {/* SOOCIAL MEDIA */}
       <div className="flex justify-center">
-        <div className="flex flex-wrap justify-center gap-4 mt-[30px] md:mt-[60px] max-w-[264px] md:max-w-none">
-          {socialMedia.map((icon) => (
+        <div className="flex flex-wrap justify-center gap-4 mt-[30px] md:mt-[60px] max-w-[264px] md:max-w-none" dir="ltr">
+          {Object.entries(socialMedia).map(([icon, link]) => (
             <a
-              key={icon}
-              href="#"
-              className="flex justify-center items-center w-[40px] md:w-[60px] h-[40px] md:h-[60px] border border-[#030303] rounded-[5px] px-[5px] py-[10px]"
+              key={link}
+              href={link}
+              className="flex justify-center items-center w-[40px] md:w-[60px] h-[40px] md:h-[60px] border border-[#030303] rounded-[5px] px-[5px] py-[10px] relative"
             >
-              <img
+              {/* @ts-ignore */}
+              <Image
                 src={`/_we-in-media/${icon}`}
+                width={40}
+                height={40}
                 alt={`${icon.split(".")[0]}`}
+                className="w-auto h-auto"
               />
             </a>
           ))}
@@ -106,7 +127,7 @@ const WeInMedia = () => {
 
         <div className="mt-[26px] md:mt-9 lg:mt-[60px] flex flex-col md:flex-row gap-5 md:gap-[48px] items-center">
           <button
-            className={`py-[16.5px] px-[12.5px] md:px-[30px] max-w-[218px] md:max-w-[328px] lg:max-h-[54px] 2xl:max-w-[342px] 2xl:max-h-[66px] flex items-center bg-[#FFDE9F] border border-[#030303] rounded-[5px] uppercase text-xs md:text-lg ${ppNeueMont500.className}`}
+            className={`py-[16.5px] px-[12.5px] md:px-[30px] max-w-[218px] md:max-w-[328px] lg:max-h-[54px] 2xl:w-[342px] 2xl:max-h-[66px] flex justify-center items-center bg-[#FFDE9F] border border-[#030303] rounded-[5px] uppercase text-xs md:text-lg ${ppNeueMont500.className}`}
           >
             {questionsIntl.raw("writeTGBtn")}{" "}
             <Image
@@ -114,19 +135,40 @@ const WeInMedia = () => {
               width={16}
               height={16}
               alt="arrow"
-              className="ml-4 w-[11.33px] md:w-[16px] h-[11.33px] md:h-[16px]"
+              className={`w-[11.33px] md:w-[16px] h-[11.33px] md:h-[16px] ${
+                locale !== "he" ? "ml-4" : "mr-4"
+              }`}
             />
           </button>
 
-          <a href="" className="flex items-center uppercase text-xs md:text-lg">
-            [{questionsIntl.raw("leaveReqLink")}{" "}
-            <Image
-              src="/arrow-side-dark.png"
-              width={16}
-              height={16}
-              alt="arrow"
-              className="mx-1 w-[11.33px] md:w-[16px] h-[11.33px] md:h-[16px]"
-            />
+          <a
+            href=""
+            className={`flex items-center uppercase text-xs md:text-lg ${
+              locale === "he" ? "flex-row-reverse" : ""
+            }`}
+          >
+            {locale === "he" && (
+              <Image
+                src="/arrow-side-dark.png"
+                width={16}
+                height={16}
+                alt="arrow"
+                className={`mx-1 w-[11.33px] md:w-[16px] h-[11.33px] md:h-[16px]`}
+              />
+            )}
+            [{questionsIntl.raw("leaveReqLink")}
+            {locale !== "he" && (
+              <>
+                {" "}
+                <Image
+                  src="/arrow-side-dark.png"
+                  width={16}
+                  height={16}
+                  alt="arrow"
+                  className={`mx-1 w-[11.33px] md:w-[16px] h-[11.33px] md:h-[16px]`}
+                />
+              </>
+            )}
             ]
           </a>
         </div>
@@ -135,14 +177,14 @@ const WeInMedia = () => {
   );
 };
 
-const socialMedia = [
-  "behance.png",
-  "youtube.png",
-  "tg.png",
-  "vk.png",
-  "inst.png",
-  "vc.png",
-  "habr.png",
-];
+const socialMedia = {
+  "behance.png": "https://www.behance.net/futuremarkt",
+  "youtube.png": "https://www.youtube.com/@futuremarkt",
+  "tg.png": "https://t.me/futuremarkt",
+  "vk.png": "https://vk.com/futuremarkt",
+  "inst.png": "https://www.instagram.com/future_markt/",
+  "vc.png": "https://vc.ru/u/1410761-futuremarkt",
+  "habr.png": "https://habr.com/ru/users/FutureMarkt/publications/articles/",
+};
 
 export default WeInMedia;
