ef6fde2d3e90d77f723135c52841fa3a1067d48d
IgorRusskikh <russkikh200@mail.ru>
2024-06-03 19:49:10 +0700

    Добавлены кастомные размеры шрифтов

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,15 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontSize: {
+      "3xl": "85px",
+      "2xl": "64px",
+      xl: "24px",
+      lg: "20px",
+      md: "18px",
+      sm: "16px",
+      xs: "14px",
+    },
   },
   plugins: [],
 };
